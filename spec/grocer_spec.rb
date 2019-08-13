b505fbf0463e79cf94dbcbc304a9bc55a8ba4032
Katherine Kelly <katherinewkwong@gmail.com>
2019-08-13 06:36:23 +0000

    Automatically backed up by Learn

--- spec/grocer_spec.rb
+++ spec/grocer_spec.rb
@@ -220,7 +220,8 @@ describe "Grocer" do
         consolidated = consolidate_cart(cart)
         coupons_applied = apply_coupons(consolidated, coupons)
         clearance_applied = apply_clearance(coupons_applied)
-
+        p "----------------------"
+        p clearance_applied
         expect(self).to receive(:consolidate_cart).with(cart).and_return(consolidated)
         expect(self).to receive(:apply_coupons).with(consolidated, coupons).and_return(coupons_applied)
         expect(self).to receive(:apply_clearance).with(coupons_applied).and_return(clearance_applied)
